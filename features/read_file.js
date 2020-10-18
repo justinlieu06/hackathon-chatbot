d90b1a461c3d1d63becfdc65f1162f827c068abf
Yuanyuan Zhou <bettinazhounyu@gmail.com>
2020-10-18 11:17:35 -0400

    Attach JSON file

--- features/read_file.js
+++ features/read_file.js
@@ -1,3 +1,6 @@
+const Q = require("q");
+const _ = require("lodash");
+const fs = require("fs");
 
 module.exports = function (controller) {
     controller.hears('menu', 'message', async (bot, message) => {
@@ -17,20 +20,55 @@ module.exports = function (controller) {
         });
     });
 
-    controller.hears('job-experience', "message,direct_message", async (bot, message) => {
+    controller.hears('job-experience', "message", async (bot, message) => {
 
         await bot.reply(message, {
             text: 'I have lots of experience, which one are you interested in?',
             quick_replies: [
                 {
                     title: "Experience 1",
-                    payload: "job-experience-1",
+                    payload: "j-1",
                 },
                 {
                     title: "Experience 2",
-                    payload: "job-experience-2"
+                    payload: "j-2"
                 }
             ]
         });
     });
+    function myBotkitMiddleware(bot, message, next) {
+      loadHears()
+        .then((hears) => {
+          if (hears && _.isArray(hears)) {
+            hears.forEach((h) => {
+              controller.hears(h.patterns, h.events, (bot, message) => {
+                bot.reply(message, h.response);
+              });
+            });
+          }
+        })
+        .fail((err) => {
+          console.error(err);
+        });
+      next();
+    }
+    
+    // read yuan.json file
+
+    function loadHears() {
+      const deferred = Q.defer();
+
+      fs.readFile("features/yuan.json", "utf8", (err, data) => {
+        if (err) {
+          deferred.reject(err);
+          return err;
+        }
+
+        deferred.resolve(JSON.parse(data));
+      });
+
+      return deferred.promise;
+    }
+
+    controller.middleware.ingest.use(myBotkitMiddleware);
 }
