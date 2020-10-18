2287fc840c90d22222edd16aa7b0c7d886e4774f
Yuanyuan Zhou <bettinazhounyu@gmail.com>
2020-10-17 20:18:50 -0400

    Welcome message

--- features/sample_echo.js
+++ features/sample_echo.js
@@ -4,13 +4,11 @@
  */
 
 module.exports = function(controller) {
+  controller.hears("sample", "message,direct_message", async (bot, message) => {
+    await bot.reply(message, "I heard a sample message.");
+  });
 
-    controller.hears('sample','message,direct_message', async(bot, message) => {
-        await bot.reply(message, 'I heard a sample message.');
-    });
-
-    controller.on('message,direct_message', async(bot, message) => {
-        await bot.reply(message, `Echo: ${ message.text }`);
-    });
-
+  controller.on("message,direct_message", async (bot, message) => {
+    await bot.reply(message, `Echo: ${message.text}`);
+  });
 }
